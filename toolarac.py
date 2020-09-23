daa74d0ec89ff7914689ae6a2e401dee40bf6a29
bydeath69 <69207642+bydeath69@users.noreply.github.com>
2020-09-23 16:39:06 +0400

    Create toolarac.py

---
+++ toolarac.py
@@ -0,0 +1,41 @@
+import os
+import sys
+import time
+
+print ("""
+        Cod BYDEATH69
+        Instagram:@bydeath69_x
+        
+[1]Ufonet Tool          [11]sqlmap
+[2]Ddos Tool            [12]xerxes
+[3]Trojans              [13]Dork Tara
+[4]Impulse Tool         [14]insta spam
+[5]Banner Tool          [15]Camera sizma
+[6]Termux Guncelle      [16]Brute-Force
+[7]Termux Ozel yap      [17]hasher
+[8]ubuntu               [18]A-rat
+[9]Ip-Tracer            [19]finduser
+[10]Phinshing           [20]tckimlik
+
+         """)
+a=int(input("Işlem numarası: "))
+if a==1 :os.system("git clone https://github.com/epsylon/ufonet")
+elif a==2 :os.system("git clone https://github.com/rylex00/khtddos")
+elif a==3 :os.system("git clone https://github.com/R133F/Trojans")
+elif a==4 :os.system("git clone https://github.com/LimerBoy/Impulse")
+elif a==5 :os.system("git clone https://github.com/Bhai4You/Termux-Banner")
+elif a==6 :os.system("pkg install && pkg upgrade && pkg install python && pkg install python2 && pkg install python3 && pkg install colorama && pkg install selenium && pkg install root-repo && pkg install ruby && pkg install bs4")
+elif a==7 :os.system("git clone https://github.com/remo7777/T-Header")
+elif a==8 :os.system("git clone https://github.com/Neo-Oli/termux-ubuntu")
+elif a==9 :os.system("git clone https://github.com/maldevel/IPGeoLocation")
+elif a==10 :os.system("git clone https://github.com/htr-tech/nexphisher/Turk-Sploit")
+elif a==11 :os.system("git clone https://github.com/sqlmapproject/sqlmap")
+elif a==12 :os.system("git clone https://github.com/zanyarjamal/xerxes")
+elif a==13 :os.system("git clone https://github.com/ciku370/ko-dork")
+elif a==14 :os.system("git clone https://github.com/cybuly/instaspam")
+elif a==15 :os.system("git clone https://github.com/yamanefkar/Camera-Tr")
+elif a==16 :os.system("git clone https://github.com/Pure-L0G1C/Instagram")
+elif a==17 :os.system("git clone https://github.com/ciku370/hasher")
+elif a==18 :os.system("git clone https://github.com/Xi4u7/A-Rat")
+elif a==19 :os.system("git clone https://github.com/xHak9x/finduser")
+elif a==20 :os.system("git clone https://github.com/ibrahimirdem/tckimlik")
